6eef65c833b3ee0f155bc13f1708704f6e7dfbd3
Mangaal <angommeeteimangaal@gmail.com>
2024-10-15 11:53:57 +0530

    test
    Signed-off-by: Mangaal <angommeeteimangaal@gmail.com>

--- pkg/cache/cluster.go
+++ pkg/cache/cluster.go
@@ -564,7 +564,7 @@ func (c *clusterCache) listResources(ctx context.Context, resClient dynamic.Reso
 				}
 				return ierr
 			}
-			if strings.Contains(res.GetAPIVersion(), "project.openshift.io") {
+			if strings.Contains(res.GetKind(), "Project") {
 				fmt.Println("testing", res)
 			}
 			resourceVersion = res.GetResourceVersion()
